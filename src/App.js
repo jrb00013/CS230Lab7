74cadc7f20dc9033999dd2734d08322b36115288
jrb00013 <156119500+jrb00013@users.noreply.github.com>
2024-03-03 04:55:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './Navbar';
+import Card from './Card';
 
 function App() {
   return (
     <div className="App">
-      
-    <h1> CS230L </h1>
-    <h2> Section - 001 </h2>
-    <p> WVU ID: 800333252</p>
-    <p> Hi I am Joe Black</p>
+      <Navbar />
+      <h1>CS230L</h1>
+      <h2>Section - 001</h2>
+      <p>WVU ID: 800333252</p>
+      <p>Hi I am Joe Black</p>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col">
+            <Card name="01" color="primary" />
+          </div>
+          <div className="col">
+            <Card name="02" color="danger" />
+          </div>
+          <div className="col">
+            <Card name="03" color="success" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
